a2320a3a0ad6e707e6bc6270ae07672281b6d64a
stephanie-mendoza189 <tepteptep231@gmail.com>
2026-07-25 09:07:46 +0800

    login

--- ConsoleApp1/GenshinCharacterBuild/Controllers/AccountController.cs
+++ ConsoleApp1/GenshinCharacterBuild/Controllers/AccountController.cs
@@ -1,34 +1,30 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace GenshinCharacterBuild.Controllers
 {
-    public class HomeController : Controller
+    public class AccountController : Controller
     {
         [HttpGet]
-        public IActionResult Index()
+        public IActionResult Login()
         {
             return View();
         }
 
         [HttpPost]
-        public IActionResult Index(string email, string password)
+        public IActionResult Login(string email, string password)
         {
-            
+           
             if (email == "user@gmail.com" && password == "12345")
             {
                 
-                return RedirectToAction("Privacy");
+                return RedirectToAction("Index", "Home");
             }
 
             
             ViewBag.ErrorMessage = "Incorrect email or password.";
             ViewBag.EnteredEmail = email;
 
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
+            
             return View();
         }
     }
--- ConsoleApp1/GenshinCharacterBuild/Program.cs
+++ ConsoleApp1/GenshinCharacterBuild/Program.cs
@@ -1,15 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -22,8 +21,8 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
+    pattern: "{controller=Account}/{action=Login}/{id?}");
+
 
 
 app.Run();
